66d0365a52130001791de04269bf7439c465c889
Christophe Eymard <christophe.eymard@sales-way.com>
2016-01-20 11:38:46 +0100

    added apply start to force beginning values

--- velocity.jsx
+++ velocity.jsx
@@ -1,10 +1,17 @@
 
 import {Atom, Controller, clonedeep} from 'carbyne';
-import v from 'velocity-animate';
+import {default as v, $} from 'velocity-animate';
 import 'velocity-animate/velocity.ui';
 
 function mkAnimation(elt, spec) {
 
+}
+
+function applyStarts(element, props) {
+	for (var x in props) {
+		if (Array.isArray(props[x]))
+			element.style[x] = props[x][props[x].length - 1];
+	}
 }
 
 // The velocity decorator ?
@@ -13,6 +20,7 @@ export function velocity(specs) {
 		if (specs.enter) {
 			atom.on('mount', (ev, created) => {
 				let enter = {e: atom.element, p: clonedeep(specs.enter), o: {duration: 200}};
+				applyStarts(atom.element, enter.p);
 				v(enter)
 			});
 		}
@@ -20,9 +28,33 @@ export function velocity(specs) {
 		if (specs.leave) {
 			atom.on('unmount:before', ev => {
 				let leave = {e: atom.element, p: clonedeep(specs.leave), o: {duration: 200}};
+				applyStarts(atom.element, leave.p);
 				return v(leave)
 			});
 		}
+
+		atom.on('destroy:before', ev => {
+			// Call remove data
+			v.Utilities.removeData(atom.element);
+			// console.dir($);
+		});
+	}
+}
+
+
+/// XXX this is not up to what we want
+export class Staggerer {
+	constructor(amount) {
+		this.last = new Date;
+		this.amount = amount;
+		this.cumulated_amount = 0;
+	}
+
+	getDelay() : integer {
+		let delta_since_last = (new Date) - this.last;
+		this.cumulated_amount = Math.max(0, this.cumulated_amount - delta_since_last);
+
+		this.last = new Date;
 	}
 }
 
